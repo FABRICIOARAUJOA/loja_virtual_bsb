9b7af424f7f84bc3e71bc3d79c26dffa600a7273
Fabricio Araujo da Silva <fabriciodevti@gmail.com>
2024-07-23 11:15:28 -0300

    commit inicial projeto loja virtual bsb

---
+++ src/main/java/academicdev/lojavirtualbsb/LojaVirtualBsbApplication.java
@@ -0,0 +1,13 @@
+package academicdev.lojavirtualbsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualBsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualBsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academicdev/lojavirtualbsb/LojaVirtualBsbApplicationTests.java
@@ -0,0 +1,13 @@
+package academicdev.lojavirtualbsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualBsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
